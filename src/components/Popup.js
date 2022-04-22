d5dfc55bf6b1aef93a07df71247176277185132e
SokolovskiyAA <sokolovsky.a.a@yandex.ru>
2022-04-22 20:46:59 +0300

    РП8

---
+++ src/components/Popup.js
@@ -0,0 +1,39 @@
+
+export default class Popup {
+    constructor(popupSelector) {
+        this._popupElement = document.querySelector(popupSelector);
+        this._handleClickClose = this._handleClickClose.bind(this);
+        this._handleEscClose = this._handleEscClose.bind(this);
+    }
+
+    open() {
+        this._popupElement.classList.add('popup_opened');
+        this._popupElement.addEventListener('mousedown', this._handleClickClose);
+        document.addEventListener('keydown', this._handleEscClose);
+    }
+
+    close() {
+        if(this._popupElement) {
+            this._popupElement.classList.remove('popup_opened');
+            this._popupElement.removeEventListener('mousedown', this._handleClickClose);
+            document.removeEventListener('keydown', this._handleEscClose);
+        }            
+    }
+
+    _handleEscClose(evt) {
+        if (evt.key == 'Escape')
+            this.close();
+    }
+
+    _handleClickClose(evt) {
+        if (evt.target.classList.contains('popup__bnt_action_close') || evt.target.classList.contains('popup')) {
+          this.close();
+        }
+      }
+
+    setEventListeners() {
+        this._popupElement.addEventListener('mousedown', this._handleClickClose);
+        document.addEventListener('keydown', this._handleEscClose);
+    }
+
+}
---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,35 @@
+
+import Popup from './Popup.js';
+
+export default class PopupWithForm extends Popup {
+    constructor(popupSelector, { submit }) {
+        super(popupSelector);
+        this._submit = submit;
+        this._form = this._popupElement.querySelector('.popup__form');
+        this._inputList = Array.from(this._form.querySelectorAll('.popup__input'));
+        this._submitHandler = this._submitHandler.bind(this);
+    }
+
+    _getInputValues() {
+        const inputValues = {};
+        this._inputList.forEach((input) => {
+            inputValues[input.name] = input.value;
+        });
+        return inputValues;
+    }
+
+    _submitHandler(evt) {
+        evt.preventDefault();
+        this._submit(this._getInputValues());
+    }
+
+    setEventListeners() {
+        this._form.addEventListener('submit', this._submitHandler);
+        super.setEventListeners();
+    }
+
+    close() {
+        this._form.reset();
+        super.close();
+    }
+}
